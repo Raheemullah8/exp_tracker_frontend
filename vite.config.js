d5036de9e69c59c84f30e2fc97d15f9ac41f6230
Kashif0955 <154120701+Raheemullah8@users.noreply.github.com>
2025-10-31 03:21:18 -0700

    change

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/",
+  base: './',  // 👈 Yeh sabse important fix hai
   plugins: [
     react(),
     tailwindcss()
   ],
-  
 })
